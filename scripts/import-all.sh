1925755a3bd15ae76cd652d446d09b18604eaaba
Andreas Kähäri <andreas.kahari@nbis.se>
2025-05-22 16:44:26 +0200

    Make it posible to re-init the database without removing volume

--- scripts/import-all.sh
+++ scripts/import-all.sh
@@ -2,5 +2,15 @@
 
 PATH=$HOME/scripts:$PATH
 
+# The database is created if it does not exist, and recreated if
+# the environment variable DATABASE_REINIT is set to some value.
+
+if [ ! -f "$DATABASE" ] || [ -n "${DATABASE_REINIT:-}" ]
+then
+	echo 'Initializing database.' >&2
+	rm -f "$DATABASE"
+	sqlite3 "$DATABASE" <sql/schema.sql
+fi
+
 import-experimentally_validated.sh \
 	/data-import/1-Experimentally_validated/Experimentally_validated_PDB_files.zip
