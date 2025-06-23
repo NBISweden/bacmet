1ae8d22331692038011d6b26b866a7c28aa612b9
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2025-06-23 16:56:34 +0200

    refactor: improve typing

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import (
+from flask import (  # type: ignore
     Flask,
     render_template,
     request,
--- app/parsers.py
+++ app/parsers.py
@@ -1,10 +1,10 @@
-from typing import Literal, Optional, Tuple
+from typing import cast, Literal, Optional, Tuple
 
 
-def database(data: str) -> Literal["validated", "predicted"]:
+def database(data: str) -> Optional[Literal["validated", "predicted"]]:
     value = None if data is None else data.lower()
     if value in {None, "validated", "predicted"}:
-        return value
+        return cast(Literal["validated", "predicted"], value)
 
     raise ValueError(f"Could not parse location: {value}")
 
@@ -16,7 +16,7 @@ def chemical_class(data: str) -> Optional[str]:
 def location(
     data: str,
     database: Literal["validated", "predicted"]
-) -> Literal["any", "chromosome"]:
+) -> Optional[Literal["any", "chromosome"]]:
     value = None if data is None else data.lower()
     allowed_values = (
         {"any", "chromosome", "plasmid"}
@@ -24,7 +24,9 @@ def location(
         else {"any", "chromosome"}
     )
     if value is None or value in allowed_values:
-        return None if value == "any" else value
+        return None if value == "any" else cast(
+            Literal["any", "chromosome"], value
+        )
 
     raise ValueError(
         f"Could not parse location: {value} for database: {database}"
@@ -36,9 +38,9 @@ def protein_description(data: str) -> Optional[str]:
 
 
 def peptide_sequence_length_range(
-    data: (str, str)
+    data: Tuple[str, str]
 ) -> Optional[Tuple[int | None, int | None]]:
-    min_len, max_len = data
-    min_len = None if not min_len else int(min_len)
-    max_len = None if not max_len else int(max_len)
+    min_len_str, max_len_str = data
+    min_len = None if not min_len_str else int(min_len_str)
+    max_len = None if not max_len_str else int(max_len_str)
     return min_len, max_len
