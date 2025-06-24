b4b3be0ec439691ceb095a304ed915ac573ca8b2
Andreas Kähäri <andreas.kahari@nbis.se>
2025-06-24 13:33:40 +0200

    Let Python code use the new "validated_compounds" table

--- app/search.py
+++ app/search.py
@@ -16,7 +16,9 @@ def find_in_validated(
         items = session.query(Validated)
         if chemical_class:
             items = items.filter(
-                Validated.compound.ilike(f"%{chemical_class}%")
+                Validated.compounds.any(
+                    Compounds.compound_name.ilike(f"%{chemical_class}%")
+                )
             )
         if protein_description:
             items = items.filter(
