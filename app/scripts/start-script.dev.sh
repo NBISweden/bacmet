341e78a8fe8ca86e60b593085e8b07bd7bd7ffd2
Andreas Kähäri <andreas.kahari@nbis.se>
2025-08-25 15:18:32 +0200

    fix: Start Caddy in production

--- app/scripts/start-script.dev.sh
+++ app/scripts/start-script.dev.sh
@@ -2,4 +2,5 @@
 
 XDG_DATA_HOME=/caddy caddy run --config ./Caddyfile &
 flask --app app/main --debug run --host 0.0.0.0 --port 5000 &
+
 wait
--- app/scripts/start-script.sh
+++ app/scripts/start-script.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-exec gunicorn -w "${APP_WORKERS:-4}" app:app -b 0.0.0.0:5000
+XDG_DATA_HOME=/caddy caddy run --config ./Caddyfile &
+gunicorn -w "${APP_WORKERS:-4}" app:app -b 0.0.0.0:5000 &
+
+wait
