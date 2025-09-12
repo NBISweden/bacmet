e4b27356f647c06993e332b4868a13f7efb1d24a
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2025-09-12 13:26:58 +0200

    fix: improve user experience for multi select when using max selections

--- frontend/bacmet/app/search/components/multi-select-field/multi-select-field.tsx
+++ frontend/bacmet/app/search/components/multi-select-field/multi-select-field.tsx
@@ -9,6 +9,7 @@ export const MultiSelectField = ({field, onChange, filterText, maxSelections}: {
   const [filterChecked, setFilterChecked] = useState<boolean>(false);
   const [current, setCurrent] = useState<unknown[]>(field.value);
   const usedValue = useMemo(() => onChange ? field.value : current, [onChange, field.value, current])
+  const usedMaxSelections = maxSelections === undefined ? field.values.length : Math.min(maxSelections, field.values.length);
   const handleChange = useCallback<ChangeEventHandler<HTMLInputElement>>((event) => {
     const singleValue: unknown  = event.target.value;
     if (singleValue !== undefined) {
@@ -17,14 +18,14 @@ export const MultiSelectField = ({field, onChange, filterText, maxSelections}: {
         ? usedValue.filter(v => v !== singleValue)
         : [...usedValue, singleValue]
       );
-      const limitedNextValue = maxSelections ? nextValue.slice(0, Math.max(0, maxSelections)) : nextValue
+      const limitedNextValue = nextValue.slice(0, Math.max(0, usedMaxSelections))
       if (onChange) {
         onChange(limitedNextValue);
       } else {
         setCurrent(limitedNextValue);
       }
     }
-  }, [maxSelections, usedValue, setCurrent, onChange]);
+  }, [usedMaxSelections, usedValue, setCurrent, onChange]);
 
   const handleFilter = useCallback<ChangeEventHandler<HTMLInputElement>>((event) => {
     const v = event.target.value;
@@ -36,21 +37,17 @@ export const MultiSelectField = ({field, onChange, filterText, maxSelections}: {
     setFilterChecked(v);
   }, [setFilterChecked]);
 
-  const filteredValues = useMemo(() => {
+  const [filteredValuesSet, usedValueSet] = useMemo(() => {
     const textFiltered = filterValue ? field.values.filter(v => v.label.toLowerCase().includes(filterValue.toLowerCase())) : field.values;
     const checkedFiltered = filterChecked ? textFiltered.filter(v => usedValue.includes(v.value)) : textFiltered;
-    return checkedFiltered;
+    return [new Set(checkedFiltered.map(v => v.value)), new Set(usedValue)];
   }, [field.values, usedValue, filterValue, filterChecked]);
 
   const handleToggleAll = useCallback<MouseEventHandler<HTMLButtonElement>>(() => {
-    const usedValueSet = new Set(usedValue);
-    const filteredValuesSet = new Set(filteredValues.map(v => v.value));
     const updatedValueSet = filteredValuesSet.difference(usedValueSet);
     const allFilteredSelected = updatedValueSet.size === 0;
-    const limitedValueSet = maxSelections === undefined
-      ? updatedValueSet
-      : new Set(Array.from(updatedValueSet).slice(0, Math.max(0, maxSelections - usedValue.length)))
-    const nextValue = allFilteredSelected
+    const limitedValueSet = new Set(Array.from(updatedValueSet).slice(0, Math.max(0, usedMaxSelections - usedValueSet.size)))
+    const nextValue = allFilteredSelected || usedValueSet.size === usedMaxSelections
       ? Array.from(usedValueSet.difference(filteredValuesSet))
       : Array.from(usedValueSet.union(limitedValueSet));
     if (onChange) {
@@ -58,7 +55,11 @@ export const MultiSelectField = ({field, onChange, filterText, maxSelections}: {
       } else {
         setCurrent(nextValue);
       }
-  }, [maxSelections, filteredValues, usedValue, setCurrent, onChange]);
+  }, [usedMaxSelections, filteredValuesSet, usedValueSet, setCurrent, onChange]);
+  const availableItems = Math.min(usedMaxSelections - usedValueSet.difference(filteredValuesSet).size, usedMaxSelections);
+  const toggleText = usedMaxSelections === field.values.length
+    ? "all"
+    : availableItems === usedMaxSelections ? `${usedMaxSelections}` : `${availableItems} of ${usedMaxSelections}`
 
   return (
     <>
@@ -79,14 +80,18 @@ export const MultiSelectField = ({field, onChange, filterText, maxSelections}: {
               <label className="form-check-label" htmlFor={filterCheckedId}>Checked only</label>
             </div>
           </div>
-          <button className="btn btn-outline-secondary" type="button" onClick={handleToggleAll}>Toggle all</button>
+          <button className="btn btn-outline-secondary" type="button" onClick={handleToggleAll}>Toggle {toggleText}</button>
         </div>
       ) : <></>}
+      {filteredValuesSet.size === field.values.length ? <></> : <div className={`row mb-3 px-3 alert alert-info ${styles.multiSelectBody}`}>
+        Showing {filteredValuesSet.size} of {field.values.length} options.
+        {maxSelections !== undefined && availableItems !== undefined && availableItems !== maxSelections ? <> Where {maxSelections - availableItems} selected options are hidden.</> : <></>}
+      </div>}
       <div className={`row mb-3 px-3 ${styles.multiSelectBody}`}>
-      {filteredValues.map(value => {
+      {field.values.map(value => {
         const valueId = `${fieldId}-${value.value}`;
         return (
-          <div key={value.value + ""} className="form-check col-md-4">
+          <div key={value.value + ""} className={`form-check col-md-4 ${filteredValuesSet.has(value.value) ? "" : "d-none"}`}>
             <input
               className="form-check-input"
               type="checkbox"
@@ -94,6 +99,7 @@ export const MultiSelectField = ({field, onChange, filterText, maxSelections}: {
               onChange={handleChange}
               checked={usedValue.includes(value.value)} 
               value={value.value + ""}
+              disabled={usedValue.length === maxSelections && !usedValue.includes(value.value) ? true : undefined}
               id={valueId}
               />
             <label
@@ -104,6 +110,7 @@ export const MultiSelectField = ({field, onChange, filterText, maxSelections}: {
         )
       })}
       </div>
+      <div className={`row mb-3 px-3`}></div>
     </>
   )
 }
