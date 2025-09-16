3c2eb76e458437050fb190ef33143a76f71a502e
Andreas Kähäri <andreas.kahari@nbis.se>
2025-09-16 15:59:32 +0200

    feat: Merge the start scirpt for the app
    The development service is invoked by passing "dev" as the 1st argument.

--- app/scripts/start-script.sh
+++ app/scripts/start-script.sh
@@ -10,4 +10,11 @@ install -d "$XDG_CONFIG_HOME"
 install -d "$XDG_DATA_HOME"
 
 caddy start --config ./Caddyfile
-exec gunicorn -w "${APP_WORKERS:-4}" app:app -b 0.0.0.0:5000
+
+if [ "${1-}" = dev ]; then
+	# Run development service.
+	exec flask --app app/main --debug run --host 0.0.0.0 --port 5000
+else
+	# Run production service.
+	exec gunicorn -w "${APP_WORKERS:-4}" app:app -b 0.0.0.0:5000
+fi
