72a488048a27f50758d979523940af83d62768b7
Andreas Kähäri <andreas.kahari@nbis.se>
2025-09-17 16:47:53 +0200

    feat: Remove separate "database" container
    Data loading is instead done by copying the data-to-be-imported into the
    running container.  The data is loaded when it is seen, then deleted and
    the service restarted.

--- app/scripts/start-script.sh
+++ app/scripts/start-script.sh
@@ -11,10 +11,68 @@ install -d "$XDG_DATA_HOME"
 
 caddy start --config ./Caddyfile
 
-if [ "${1-}" = dev ]; then
-	# Run development service.
-	exec flask --app app/main --debug run --host 0.0.0.0 --port 5000
-else
-	# Run production service.
-	exec gunicorn -w "${APP_WORKERS:-4}" app:app -b 0.0.0.0:5000
-fi
+export DATABASE="$HOME/data/database.db"
+export IMPORT_DIR="$HOME/data/data-import"
+
+while true; do
+	if [ ! -s "$DATABASE" ]; then
+		# If the database file does not exist, import data.
+		echo 'Database file does not exist, importing data.' >&2
+		do_import=true
+	elif [ -d "$IMPORT_DIR" ]; then
+                # If the import directory exists, import data.  This
+                # allows users to drop new data into the import
+                # directory to replace the existing database.
+		echo 'Import directory exists, (re-)importing data.' >&2
+		do_import=true
+	fi
+
+	if "${do_import:-false}"; then
+		# Wait for the import directory to exist.
+		while [ ! -d "$IMPORT_DIR" ]; do
+			printf 'Waiting for import directory "%s" to exist...\n' \
+				"$IMPORT_DIR" >&2
+			sleep 5
+		done
+
+                # Sleep for 30 seconds to give data a chance to be
+                # copied in.
+		echo 'Sleeping for 30s...' >&2
+		sleep 30
+
+                # Import data into a temporary database, then switch it
+                # to be the active one.  Also, remove the uploaded raw
+                # data.
+		DATABASE=$DATABASE.new db-scripts/scripts/import-all.sh
+		mv "$DATABASE.new" "$DATABASE"
+		rm -rf "$IMPORT_DIR"
+
+		unset -v do_import
+
+		# Restart the service if it is running.
+		if [ -n "${service_pid-}" ] && kill -0 "$service_pid" 2>/dev/null
+		then
+			echo 'Restarting service...' >&2
+			kill "$service_pid"
+			wait "$service_pid"
+		fi
+		unset -v service_pid
+	fi
+
+	# Start the service if it is not running.
+	if [ -z "${service_pid-}" ] || ! kill -0 "$service_pid" 2>/dev/null
+	then
+		echo 'Starting service...' >&2
+
+		if [ "${1-}" = dev ]; then
+			# Run development service.
+			flask --app app/main --debug run --host 0.0.0.0 --port 5000 &
+		else
+			# Run production service.
+			gunicorn -w "${APP_WORKERS:-4}" app:app -b 0.0.0.0:5000 &
+		fi
+		service_pid=$!
+	fi
+
+	sleep 60
+done
--- database/scripts/import-all.sh
+++ database/scripts/import-all.sh
@@ -2,35 +2,39 @@
 
 set -u
 
-PATH=$HOME/scripts:$PATH
+PATH=$HOME/db-scripts/scripts:$PATH
 
 # The database is created if it does not exist, and recreated if
 # the environment variable DATABASE_REINIT is set to some value.
 
-if [ ! -f "$DATABASE" ] || [ -n "${DATABASE_REINIT:-}" ]
-then
+if [ ! -f "$DATABASE" ]; then
 	echo 'Initializing database.' >&2
 	rm -f "$DATABASE"
-	sqlite3 "$DATABASE" <sql/schema.sql
+	sqlite3 "$DATABASE" <"$HOME/db-scripts/sql/schema.sql"
+fi
+
+# We expect to see the data to import in the directory called "import"
+# in the same directory where the database is stored.
+
+if [ ! -d "$IMPORT_DIR" ]; then
+	printf 'Failed to import data from "%s"\n' "$IMPORT_DIR" >&2
+	exit 1
 fi
 
 import-validated-compounds.sh \
-	/data-import/1-Experimentally_validated/2-BacMet_compound_CAS_numbers.csv
+	"$IMPORT_DIR"/1-Experimentally_validated/2-BacMet_compound_CAS_numbers.csv
 
 import-validated.sh \
-	/data-import/1-Experimentally_validated/1-BacMet_experimentally_validated_database_3.0.csv
+	"$IMPORT_DIR"/1-Experimentally_validated/1-BacMet_experimentally_validated_database_3.0.csv
 
 import-validated-pdb-files.sh \
-	/data-import/1-Experimentally_validated/Experimentally_validated_PDB_files.zip
+	"$IMPORT_DIR"/1-Experimentally_validated/Experimentally_validated_PDB_files.zip
 
 import-predicted-unique-homologues.sh \
-	/data-import/2-Predicted_database/1-Predicted_unique_homologues.zip
+	"$IMPORT_DIR"/2-Predicted_database/1-Predicted_unique_homologues.zip
 
 import-predicted-groups.sh \
-	/data-import/2-Predicted_database/3-Predicted_groups.zip
+	"$IMPORT_DIR"/2-Predicted_database/3-Predicted_groups.zip
 
 import-sensitivity-distributions.sh \
-	/data-import/3-Sensitivity_distributions/MIC_*.zip
-
-#echo 'Vacuuming database...' >&2
-#sqlite3 "$DATABASE" vacuum
+	"$IMPORT_DIR"/3-Sensitivity_distributions/MIC_*.zip
